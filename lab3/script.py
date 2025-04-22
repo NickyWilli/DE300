df09e15cd8b24c9a356556c3b546371f9f42ca2f
Ubuntu <ubuntu@ip-10-192-10-91.ec2.internal>
2025-04-22 23:31:36 +0000

    created ec2_output.csv

--- lab3/script.py
+++ lab3/script.py
@@ -1,10 +1,13 @@
+import os
 import pandas as pd
+
+# Ensure the output folder exists
+os.makedirs('output', exist_ok=True)
 
 df = pd.DataFrame({
     'Name': ['Alice', 'Bob'],
     'Age': [25, 30]
 })
 
-df.to_csv('output.csv', index=False)
+df.to_csv('output/ec2_output.csv', index=False)
 print("CSV file created!")
-
